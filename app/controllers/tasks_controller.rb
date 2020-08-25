88a198fc43063a2e843fb1164c438ca47e6d345f
issei.suzuki <issei.karate@gmail.com>
2020-08-25 07:08:31 +0000

    add pagination

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
- def index
-  @tasks = Task.all
-end
+  def index
+    @messages = Message.all.page(params[:page]).per(3)
+  end
 
 def show
   set_task
