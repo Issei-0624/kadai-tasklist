1252bde85f2159e31b74c473ece43d1cba17ec29
issei.suzuki <issei.karate@gmail.com>
2020-08-27 01:18:43 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,15 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+    if logged_in?
+      @task = current_user.tasks.build  
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
+    end
   end
-
+  
 def show
   set_task
 end
@@ -16,11 +20,12 @@ end
 
   def create
       @task = Task.new(task_params)
-      
+      @task = current_user.tasks.build(task_params)
       if @task.save
           flash[:success] = "Taskが正常に投稿されました"
           redirect_to @task
       else
+        @task = current_user.tasks.build(task_params).page(params[:page])
           flash.now[:danger] = "Taskが正常に投稿されませんでした"
           render :new
       end
@@ -57,7 +62,13 @@ end
   
   def task_params
       params.require(:task).permit(:content, :status)
-      
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
 
